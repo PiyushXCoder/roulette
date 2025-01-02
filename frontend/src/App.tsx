4900ec50d6881f5cb3a041387bc0821c759270cf
PiyushXCoder <piyush.raj.kit@gmail.com>
2025-01-02 15:53:59 +0530

    ref(frontend): Draw funtion in args

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,31 @@
-import { GameScreen } from "./components/game-screen/game-screen.tsx"
+import { GameScreen, ScreenContext } from "./components/game-screen/game-screen.tsx"
 import "./App.css";
+
+let x = 0;
+
+function draw(deltaSeconds: number, context: CanvasRenderingContext2D, screenContext: ScreenContext) {
+  console.log(deltaSeconds);
+  context.clearRect(0, 0, 1000 + 4, 500 + 4);
+  context.fillStyle = "#f00";
+
+  if (screenContext.events.mouse.down) {
+    context.fillRect(0, 0, 100, 100);
+  }
+
+  context.fillStyle = "#0f0";
+  context.fillRect(x, 300, 50, 50);
+  x += 50 * deltaSeconds;
+
+  context.fillStyle = "#00f";
+  context.fillRect(screenContext.events.mouse.x - 8, screenContext.events.mouse.y - 8, 16, 16);
+}
+
+export { GameScreen };
+
 
 function App() {
   return (
-    <GameScreen />
+    <GameScreen draw={draw} />
   )
 }
 
--- frontend/src/components/game-screen/game-screen.tsx
+++ frontend/src/components/game-screen/game-screen.tsx
@@ -61,12 +61,14 @@ let screenContext: ScreenContext = {
   screen: {
     width: 0,
     height: 0
-  }
+  },
 };
 
 interface Config {
   width?: number,
   height?: number
+
+  draw(deltaSeconds: number, context: CanvasRenderingContext2D, screenContext: ScreenContext): void
 }
 
 function GameScreen(args: Config) {
@@ -98,7 +100,7 @@ function GameScreen(args: Config) {
     const handleDraw = (timestamp: number) => {
       let deltaSeconds = (timestamp - oldTimeStamp) / 1000;
       oldTimeStamp = timestamp;
-      draw(deltaSeconds, context, screenContext);
+      args.draw(deltaSeconds, context, screenContext);
       window.requestAnimationFrame(handleDraw);
     }
 
@@ -106,19 +108,7 @@ function GameScreen(args: Config) {
 
   }, [canvasRef]);
 
-  return <canvas ref={canvasRef} style={{ border: "green 2px solid", imageRendering: "pixelated", width: args.width || 1000 + "px", height: args.height || 500 + "px" }} />
+  return <canvas ref={canvasRef} style={{ imageRendering: "pixelated", width: args.width || 1000 + "px", height: args.height || 500 + "px" }} />
 }
 
-function draw(deltaSeconds: number, context: CanvasRenderingContext2D, screenContext: ScreenContext) {
-  console.log(deltaSeconds);
-  context.clearRect(0, 0, 1000 + 4, 500 + 4);
-  context.fillStyle = "#f00";
-
-  if (screenContext.events.mouse.down) {
-    context.fillRect(0, 0, 100, 100);
-  }
-
-  context.fillRect(screenContext.events.mouse.x - 8, screenContext.events.mouse.y - 8, 16, 16);
-}
-
-export { GameScreen };
+export { GameScreen, type ScreenContext };
